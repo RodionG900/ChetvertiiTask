9408cdf63133b2bbf5633acf8274408f26d64fc2
RodionG900 <65422572+RodionG900@users.noreply.github.com>
2020-11-30 07:00:59 +0300

    Add files via upload

---
+++ ChetvertiiTask.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    if (str.find(".") != string::npos) str.erase(str.find("."), 1);
+    while (str.find("  ") != string::npos) {
+        str.erase(str.find("  "), 1);
+    }
+    if (str.at(0) == ' ') str.erase(0, 1);
+    if (str.at(str.length() - 1) == ' ') str.erase(str.length() - 1, 1);
+    cout << endl << endl << "ќбработанна€ строка:\n";
+    cout << str << endl;
+
+    string subend = " " + str.substr(str.rfind(" ") + 1) + " ";
+    str = " " + str + " ";
+    while (str.find(subend) != string::npos) str.erase(str.find(subend), subend.size() - 1);
+    if (str.at(0) == ' ') str.erase(0, 1);
+    if (str.at(str.length() - 1) == ' ') str.erase(str.length() - 1, 1);
+    str += subend;
+    if (str.at(str.length() - 1) == ' ') str.erase(str.length() - 1, 1);
+    cout << endl << endl << "Cтрока без вхождений последнего слова:\n";
+    cout << str;
+
+    cout << endl << endl << "3) ≈сли перва€ буква слова входит в это слово еще раз.(Сabcad as aaaТ->Тabcad aaaТ) :\n";
+    str += " ";
+    while (str.find(" ") != string::npos) {
+        string sub = str.substr(0, str.find(" "));
+        bool l = 0;
+        for (auto b : sub.substr(1)) {
+            if (b == sub[0]) l = 1;
+        }
+        if (l) cout << sub << " ";
+        str = str.substr(str.find(" ") + 1);
+    }
+    cout << "\n\n\n";
+}
